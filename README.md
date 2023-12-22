62389bb57a4e4410d776b1e89016820bf8ca4516
Richard <rdcoope@gmail.com>
2023-12-22 13:08:01 -0600

    Follow along with reading page

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # javascript
+Basic notes and new commands learned. 
+
+Standard practice is to use camelCase 
+camelCase is where the first letter of word 
+2+ is capitalized. 
+
+When hard coding a constant variable use all caps
+with words seperated by underscores. This way you
+you can differentiate what is hard coded and what
+is system assigned.
+
+************* Commands learned **************
+
+console.log() -- creates a console message in the browser
+
+alert() -- displays an error message that requires
+user intervention to continue.
+
+let -- declare a variable that can be changed
+
+const -- decalre a variable that CANNOT be changed
--- javascript.js
+++ javascript.js
@@ -1,1 +1,41 @@
 console.log("What is this? An external javascript?")
+
+// 2 slashes is a comment
+//variable declarations
+
+
+let message;
+
+message = 'Hello'; // store the string 'Hello' in the variable named message
+console.log(message); // Hello!
+
+//multiple variables in 1 line
+let user = 'John', age = 25 ;
+
+//changing variables
+message = 'Hello!'
+console.log(message)
+message = 'World'
+console.log(message)
+
+// using variables with other variables
+let hello = 'Hello World!';
+message = hello;
+console.log(message)
+
+// use cont to create a variable that cannot be changed
+const myBirthday = '22.09.1992';
+//myBirthday = '01.0100.010.010'; // will produce an error
+
+
+
+// Use const for web hex codes
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
